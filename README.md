44c673d345fca0cd2cfdff9a67fd07cd8e3866eb
Steve Schrab <steve.schrab@gsdesign.com>
2015-02-03 11:37:35 -0600

    Initial Aplha code 0.0.1

--- README.md
+++ README.md
@@ -1,2 +1,70 @@
-navinator
+Navinator
 =========
+
+Creates a "mobile" style nav from your "desktop" style nav.
+
+#### Requires
+
+jQuery 1.11.0 or higher
+
+#### Example
+
+Add the Navinator JavaScript file before the closing body tag. Add the Navinator CSS file in the head tag. The CSS is purposely minimal to make styling easy without needing to override a bunch of attributes.
+
+Make sure your HTML page content is wrapped in a `div` ...
+
+```html
+<body>
+  <div id="page-container">
+    _(html content)_
+  </div>
+</body>
+```
+
+... also be sure have a triggering element.
+
+```html
+<button id="navinator-button">Menu</button>
+```
+
+Finally you just need a nav to "navinate."
+
+```html
+<nav id="my-main-nav">
+  <a href="#">Some Links</a>
+</nav>
+```
+
+Then just initialize with:
+
+```javascript
+$('#my-main-nav').navinator();
+ ```
+
+Enjoy your navinated Nav!
+
+You can also set different options like so:
+
+```javascript
+$('#my-main-nav').navinator({
+	direction: 'right',
+	minWidth: 768,
+	pageElement: '.my-page-wrapper',
+	buttonElement: '.my-button'
+});
+ ```
+
+#### Options
+
+Option | Type | Default | Description
+------ | ---- | ------- | -----------
+direction | string | 'left' | Change the side of the screen the menu opens on. 'right' or 'left'
+minWidth | integer | 0 | The minimum browser width to show Navinator. If it's larger, it switches back to your initially defined Nav. By default, there is no minimum width and Navinator is always shown.
+pageElement | string | '#page-container' | Defines which element to use as the page wrapper
+buttonElement | string | '#navinator-button' | Defines which element to use as the toggle menu button
+
+#### TODO
+
+- reset or resize open nav on window resize
+- add touch/swipe support
+- multi-level navigation
---
+++ src/navinator.js
@@ -0,0 +1,232 @@
+/* global console, window, module, define, jQuery, require */
+/*
+ * Navinator v0.0.1
+ * https://github.com/gsmke/navinator
+ */
+
+;(function(factory) {
+    'use strict';
+    if (typeof define === 'function' && define.amd) {
+        define(['jquery'], factory);
+    } else if (typeof exports !== 'undefined') {
+        module.exports = factory(require('jquery'));
+    } else {
+        factory(jQuery);
+    }
+
+}(function($) {
+
+    'use strict';
+    var Navinator = window.Navinator || {};
+
+    Navinator = (function () {
+        function Navinator(element, settings) {
+
+            var _ = this;
+            _.defaults = {
+                buttonElement: '#navinator-button',
+                direction: 'left',
+                minWidth: 0,
+                navZ: 'bottom',
+                pageElement: '#page-container'
+            };
+
+            _.options = $.extend({}, _.defaults, settings);
+
+            // Navinator Globals
+            _.$navElement = $(element);
+            _.$pageElement = $(_.options.pageElement);
+            _.$buttonElement = $(_.options.buttonElement);
+            _.navCreated = false;
+
+            // Check if the page wrapper and button elements are present
+            if (_.$pageElement.length === 0) {
+                console.error('%cNavinator Error: pageElement doesn\'t exist', 'font-weight: bold');
+            } else if (_.$buttonElement.length === 0) {
+                console.error('%cNavinator Error: buttonElement doesn\'t exist', 'font-weight: bold');
+            } else {
+                _.init();
+            }
+        }
+
+        return Navinator;
+
+    }());
+
+    // Changes the classnames and IDs on the HTML to be cloned. Prevents CSS conflicts.
+    Navinator.prototype.addPrefix = function($el) {
+
+        $el.find('*').each(function (index, el) {
+            var $el = $(el);
+
+            var elementId = $el.attr('id'),
+                elementClass = $el.attr('class');
+
+            if(typeof elementId === 'string' && '' !== elementId) {
+                $el.attr('id', elementId.replace(/([A-Za-z0-9_.\-]+)/g, 'navinator-$1'));
+            }
+            if(typeof elementClass === 'string' && '' !== elementClass) {
+                $el.attr('class', elementClass.replace(/([A-Za-z0-9_.\-]+)/g, 'navinator-$1'));
+            }
+        });
+
+    };
+
+    // Create the new Nav
+    Navinator.prototype.create = function () {
+
+        var _ = this;
+
+        if (!_.navCreated) {
+            $('body').append('<div id="navinator-menu"></div>');
+            _.$navElement.clone().appendTo($('#navinator-menu'));
+            _.addPrefix($('#navinator-menu'));
+
+            // Check options
+            if (_.options.direction === 'right') {
+                _.$pageElement.css('right', 0);
+            } else {
+                _.$pageElement.css('left', 0);
+            }
+
+            if (_.options.navZ === 'top') {
+                $('#navinator-menu').addClass('navinator-z-top');
+            } else {
+                $('#navinator-menu').addClass('navinator-z-bottom');
+            }
+
+            _.$buttonElement.click(function (ev) {
+                ev.preventDefault();
+                _.menuToggle();
+            });
+
+            _.$navElement.hide();
+            _.$buttonElement.show();
+
+            if (!_.$pageElement.hasClass('navinator-initialized')) {
+                _.$pageElement.addClass('navinator-initialized');
+                console.log('%cNavinator Initialized!', 'font-size: 1.1em; font-weight: bold; color: green');
+            }
+
+            _.navCreated = true;
+        }
+
+    };
+
+    // Destory the new Nav
+    Navinator.prototype.destroy = function () {
+
+        var _ = this;
+
+        if (_.navCreated) {
+            $('html').removeClass('navinator-open');
+            $('#navinator-menu').remove();
+
+            _.$navElement.show();
+            _.$buttonElement.hide();
+
+            _.$pageElement.removeClass('navinator-initialized').removeAttr('style');
+            console.log('%cNavinator Destroyed!', 'font-weight: bold; color: orange');
+
+            _.navCreated = false;
+        }
+
+    };
+
+    // Update widths based on the window size
+    Navinator.prototype.updateMeasurements = function () {
+
+        var _ = this;
+        var $win = $(window);
+
+        _.winWidths[0].winWidth = $win.width();
+        _.winWidths[0].openWidth = $win.width() - 50;
+        _.winWidths[0].viewWidth = window.outerWidth;
+        _.winWidths[0].winHeight = $win.height();
+
+        _.$pageElement
+            .width(_.winWidths[0].winWidth)
+            .css('min-height', _.winWidths[0].winHeight);
+
+        if ($('html').hasClass('navinator-open')) {
+            console.log(_.winWidths[0].openWidth);
+            if (_.options.direction === 'right') {
+                _.$pageElement.css('right', -_.winWidths[0].openWidth);
+            } else {
+                _.$pageElement.css('left', -_.winWidths[0].openWidth);
+            }
+        }
+
+    };
+
+    // Open/Close the Nav
+    Navinator.prototype.menuToggle = function () {
+
+        var _ = this;
+
+        if ($('html').hasClass('navinator-open')) {
+            // Close Menu
+            if (_.options.direction === 'right') {
+                _.$pageElement.css('right', 0);
+            } else {
+                _.$pageElement.css('left', 0);
+            }
+            $('html').removeClass('navinator-open');
+        } else {
+            // Open Menu
+            if (_.options.direction === 'right') {
+                _.$pageElement.css('right', -_.winWidths[0].openWidth);
+            } else {
+                _.$pageElement.css('left', -_.winWidths[0].openWidth);
+            }
+            $('html').addClass('navinator-open');
+        }
+
+    };
+
+    // Initial the nav
+    Navinator.prototype.init = function () {
+
+        var _ = this;
+
+        _.winWidths = [];
+        _.winWidths.push({
+            'winWidth': 0,
+            'winHeight': 0,
+            'openWidth': 0,
+            'viewWidth': 0
+        });
+
+        _.$buttonElement.hide();
+
+        _.resizeWindow = function () {
+
+            _.updateMeasurements();
+
+            // If set, minWidth shows/hides the nav based on the size of the browser
+            // If minWidth not set, nav will always show
+            if (_.options.minWidth >= _.winWidths[0].viewWidth || _.options.minWidth === 0) {
+                _.create();
+            } else {
+                _.destroy();
+            }
+        };
+
+        // Run function on window resize
+        $(window).resize(_.resizeWindow);
+        // Run function the first time
+        _.resizeWindow();
+
+    };
+
+    // jQuery Plugin
+    $.fn.navinator = function (options) {
+
+        var _ = this;
+
+        return _.each(function (index, element) {
+            element.navinator = new Navinator(element, options);
+        });
+
+    };
+}));
